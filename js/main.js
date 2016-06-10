9b4ab30b2ce2853de865ac2396e3c2da5ad0fb33
Kate Grytsenko <aktinijal@gmail.com>
2016-06-10 15:48:39 +0300

    add slider in customers section

--- js/main.js
+++ js/main.js
@@ -18,4 +18,46 @@ $(document).ready(function(){
             menu.removeAttr('style');
         }
     });
+    
+    $('.customers-wrap').slick({
+      dots: true,
+      infinite: false,
+      speed: 300,
+      slidesToShow: 5,
+      slidesToScroll: 5,
+//      centerMode: true,
+//      centerPadding: '60px',
+      prevArrow: $('.prev'),
+      nextArrow: $('.next'),
+      responsive: [
+        {
+          breakpoint: 1024,
+          settings: {
+            slidesToShow: 3,
+            slidesToScroll: 3,
+            infinite: true,
+            dots: true
+          }
+        },
+        {
+          breakpoint: 600,
+          settings: {
+            slidesToShow: 2,
+            slidesToScroll: 2
+          }
+        },
+        {
+          breakpoint: 480,
+          settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1
+          }
+        }
+        // You can unslick at a given breakpoint now by adding:
+        // settings: "unslick"
+        // instead of a settings object
+      ]
+    });
+    
+    
 });
